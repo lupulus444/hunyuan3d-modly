e8cb9878cf79dfa211441bc4fee2288526b21c13
lupulus444 <charlie.monier444@gmail.com>
2026-04-17 19:05:33 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,9 +1,10 @@
 class Generator:
     def generate(self, image=None, prompt=None):
-        print("Input image:", image)
-        print("Prompt:", prompt)
-
-        with open("output.obj", "w") as f:
+        output_path = "output.obj"
+        
+        with open(output_path, "w") as f:
             f.write("# fake 3D model")
 
-        return "output.obj"
+        return {
+            "model": output_path
+        }
