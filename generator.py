50d33f100e311671c0e6704bb21966c9373a6373
lupulus444 <charlie.monier444@gmail.com>
2026-04-17 19:07:23 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,7 +1,11 @@
 class Generator:
+    def __init__(self, *args, **kwargs):
+        # accepte n'importe quels arguments (important)
+        pass
+
     def generate(self, image=None, prompt=None):
         output_path = "output.obj"
-        
+
         with open(output_path, "w") as f:
             f.write("# fake 3D model")
 
